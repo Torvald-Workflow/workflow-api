4ca4406082c37ccbaf6a62d4722cc8ffeb0cb90d
Quentin LAURENT <ql2697@gmail.com>
2023-01-06 22:33:41 +0100

    :tada: Initialized project with

---
+++ README.md
@@ -0,0 +1,9 @@
+# Workflow API
+
+This is the API of the Workflow's project, used to connect every part of the project together.
+
+> Todo: add a description of the project
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Workflow API')
+    .setDescription('The Workflow API description')
+    .setVersion('1.0')
+    .addTag('workflow')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
