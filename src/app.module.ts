d3ab83b82ccc12392f7e2626fea0d8dc11a22abd
Quentin LAURENT <ql2697@gmail.com>
2023-01-15 11:29:21 +0100

    :construction: Started adding mysql with typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import process from 'process';
 import configuration, { validationSchema } from './config/configuration';
 import { UsersModule } from './users/users.module';
 
@@ -9,6 +11,13 @@ import { UsersModule } from './users/users.module';
     ConfigModule.forRoot({
       load: [configuration],
       validationSchema: validationSchema,
+    }),
+    TypeOrmModule.forRoot({
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
     }),
   ],
   controllers: [],
