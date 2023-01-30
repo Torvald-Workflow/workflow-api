7a07a0736f584b2ec52ec56e60fd005bd1251b5c
Quentin 'Torvald' LAURENT <ql2697@gmail.com>
2023-01-30 19:56:04 +0100

    :bug: Corrected wrong process import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import process from 'process';
 import configuration, { validationSchema } from './config/configuration';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    UsersModule,
     ConfigModule.forRoot({
       load: [configuration],
+      isGlobal: true,
       validationSchema: validationSchema,
     }),
     TypeOrmModule.forRoot({
+      type: 'mysql',
       host: process.env.DATABASE_HOST,
       port: parseInt(process.env.DATABASE_PORT),
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.NODE_ENV === 'development',
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
