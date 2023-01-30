731811517cd27a2b128b989fa3583a3f02c373da
Quentin 'Torvald' LAURENT <ql2697@gmail.com>
2023-01-30 20:43:16 +0100

    :wrench: Synchronize the database when developping application

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { UsersModule } from './users/users.module';
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: process.env.NODE_ENV === 'development',
+      synchronize: process.env.NODE_ENV !== 'production',
     }),
     UsersModule,
   ],
