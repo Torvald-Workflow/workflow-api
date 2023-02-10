b00258f5bb301b6417b2bc1bb6cb3c0e1972c74b
Quentin LAURENT <ql2697@gmail.com>
2023-02-10 06:21:23 +0100

    :wrench: Corrected configuration of application and added docs

---
+++ src/users/dtos/EditUserDto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class EditUserDto {
+  @ApiProperty({
+    description: 'The first name of a user',
+    example: 'John',
+    type: String,
+  })
+  readonly firstName: string;
+
+  @ApiProperty({
+    description: 'The last name of a user',
+    example: 'Doe',
+  })
+  @IsString()
+  readonly lastName: string;
+
+  @ApiProperty({
+    description: 'The email of a user',
+    example: 'john.doe@domain.com',
+  })
+  @IsString()
+  readonly email: string;
+
+  @ApiProperty({
+    description: 'The state of the user',
+  })
+}
--- src/users/entity/user.entity.ts
+++ src/users/entity/user.entity.ts
@@ -1,42 +1,20 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { AbstractEntity } from 'src/global/entity/AbstractEntity';
 import { Column, Entity } from 'typeorm';
 
 @Entity({ name: 'users' })
 export class UserEntity extends AbstractEntity {
-  @ApiProperty({
-    description: 'The first name of a user',
-    example: 'John',
-  })
   @Column()
   firstName: string;
 
-  @ApiProperty({
-    description: 'The last name of a user',
-    example: 'Doe',
-  })
   @Column()
   lastName: string;
 
-  @ApiProperty({
-    description: 'The email of a user',
-    example: 'john.doe@domain.com',
-  })
   @Column()
   email: string;
 
-  @ApiProperty({
-    description: 'The password of a user',
-    example: 'password',
-  })
   @Column()
   password: string;
 
-  @ApiProperty({
-    description: 'Active status of a user',
-    example: true,
-    default: true,
-  })
   @Column({ default: true })
   isActive: boolean;
 }
