49beddaff291a885d467a12b290c87cdfb74996f
Quentin LAURENT <ql2697@gmail.com>
2023-10-22 09:57:07 +0200

    🎨 Added missing double quotes

--- scripts/generate-keypair.sh
+++ scripts/generate-keypair.sh
@@ -1,8 +1,10 @@
-SCRIPT_PATH=$(readlink -f $0)
-KEY_PATH=$(dirname $SCRIPT_PATH)/../keys
-PRIVATE_KEY_PATH=$KEY_PATH/private.key
-PUBLIC_KEY_PATH=$KEY_PATH/public.key
+#!/bin/bash
 
-ssh-keygen -t rsa -b 4096 -m PEM -f $PRIVATE_KEY_PATH -q -N ""
-openssl rsa -in $PRIVATE_KEY_PATH -pubout > $PUBLIC_KEY_PATH
-rm $PRIVATE_KEY_PATH.pub
+SCRIPT_PATH=$(readlink -f $0)
+KEY_PATH="$(dirname $SCRIPT_PATH)/../keys"
+PRIVATE_KEY_PATH="$KEY_PATH/private.key"
+PUBLIC_KEY_PATH="$KEY_PATH/public.key"
+
+ssh-keygen -t rsa -b 4096 -m PEM -f "$PRIVATE_KEY_PATH" -q -N ""
+openssl rsa -in "$PRIVATE_KEY_PATH" -pubout > "$PUBLIC_KEY_PATH"
+rm "$PRIVATE_KEY_PATH.pub"
